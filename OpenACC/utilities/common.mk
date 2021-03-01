027a0dce12e4aa710c4f0807001a39b7b5a7a387
Alexandre Amory <amamory@gmail.com>
2021-03-01 20:24:28 +0100

    stencils/jacobi-2d-imper working in xavier

--- OpenACC/utilities/common.mk
+++ OpenACC/utilities/common.mk
@@ -2,7 +2,6 @@ INCPATHS = -I$(UTIL_DIR)
 
 BENCHMARK = $(shell basename `pwd`)
 EXE = $(BENCHMARK)
-OBJ = rose_$(BENCHMARK).c
 SRC = $(BENCHMARK).c $(UTIL_DIR)/polybench.c
 HEADERS = $(BENCHMARK).h $(UTIL_DIR)/polybench.h
 
@@ -15,18 +14,15 @@ all : exe
 
 exe : $(EXE)
 
-$(OBJ) : $(SRC)
-	$(ACC) $(ACCFLAGS) $(ACC_INC_PATH) $(INCPATHS) $^
-
-$(EXE) : $(OBJ) $(BENCHMARK)-data.c 
-	$(CC) -o $@ $(CFLAGS) $(ACC_INC_PATH) $(ACC_LIB_PATH) $(INCPATHS) $^ $(ACC_LIBS)
+$(EXE) : $(SRC)
+	$(CC) $(CFLAGS) $(ACCFLAGS)  $(ACC_INC_PATH) $(ACC_LIB_PATH) $(INCPATHS) $^ $(ACC_LIBS)
 
 check: exe
 	./$(EXE)
 
 clean :
-	-rm -vf __hmpp* -vf $(EXE) *~ 
-	-rm -rf rose_$(BENCHMARK).c $(BENCHMARK)-data.c $(BENCHMARK).cl $(BENCHMARK)
+	-rm -rf __hmpp* $(EXE) a.out *~ 
+	-rm -rf Makefile.dep
 
 veryclean : clean
 	-rm -vf $(DEPS)
